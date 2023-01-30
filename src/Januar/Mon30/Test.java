e01f72b8391079e5d22352c27650f99c2a7bb1d6
tequilcaCat <oliver-steck@gmx.de>
2023-01-30 16:04:54 +0100

    Test

--- src/Januar/Mon30/Test.java
+++ src/Januar/Mon30/Test.java
@@ -1,5 +1,11 @@
+package Januar.Mon30;
+
 public class Test {
 
+    /**
+     * Dies ist ein Test
+     * @param args
+     */
     public static void main(String[] args) {
         System.out.println("Test");
     }
